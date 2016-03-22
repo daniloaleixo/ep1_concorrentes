6f4f108c988026cd976abca38d024eb401e3726d
Danilo <danilo_aleixo@hotmail.com>
2016-03-21 21:21:06 -0300

    tentando fazer o frok

--- ep1.c
+++ ep1.c
@@ -13,8 +13,6 @@
 #include "integral_sec_x.h"
 #include "agulha.h"
 #include <pthread.h>
-#include <time.h>
-#include <math.h>
 
 #define TRUE 1
 #define FALSE 0
@@ -26,6 +24,7 @@ int main(int argc, char *argv[])
 	int *v;
 	int m, n, r, s;
 	double x = 0.5;
+	pid_t pid;
 
 	if(argc >= 4)
 	{
@@ -43,24 +42,59 @@ int main(int argc, char *argv[])
 
 
 
-		/* primeira parte */
+		if((pid = fork()) < 0) {
+	    	perror("falha na criação de processo!");
+	        exit(1);
+	    } else if(pid == 0) {
+	    	
+	    	v = criaVetor(m);
+			printf("P1: Vetor:          "); imprimeVetor(v, m);
+
+			sleep(1);
+
+			heapsort(v, m);
+			printf("P1: Vetor ordenado: "); imprimeVetor(v, m);
+
+	    } else {
+	        /*waitpid(pid, NULL, 0);*/
+
+	        /* segunda parte */
+			Fibonacci(n, 0);
+
+			/* terceira parte */
+			agulhaBuffon(r);
+
+			/* quarta parte */
+			integralSecX(x, s);
+
+			waitpid(pid, NULL, 0);
+	    }
+
+		/* primeira parte *
 		v = criaVetor(m);
-		printf("P1: Vetor:          ");
-		imprimeVetor(v, m);
+		printf("P1: Vetor:          "); imprimeVetor(v, m);
 
 		heapsort(v, m);
-		printf("P1: Vetor ordenado: ");
-		imprimeVetor(v, m);
+		printf("P1: Vetor ordenado: "); imprimeVetor(v, m); */
 
 
-		/* segunda parte */
+		/* segunda parte *
 		Fibonacci(n, 0);
 
-		/* terceira parte */
+		/* terceira parte *
 		agulhaBuffon(r);
 
-		/* quarta parte */
+		/* quarta parte *
 		integralSecX(x, s);
+
+
+
+		/* 
+		primeiro aproach 
+		forkProcess(1, m, 0);
+		forkProcess(2, n, 0);
+		forkProcess(3, r, 0);
+		forkProcess(4, s, x); */
 	}
 	else {
 		printf("\n\nModo de uso\n\n");
@@ -77,5 +111,48 @@ int main(int argc, char *argv[])
 }
 
 
+/*
+void forkProcess (int process, int arg, double sec_arg)
+ {
+    int i;
+    pid_t pid;
+    int *v;
+
+    pid = fork();
+
+    if (pid == -1) {
+        /* error handling here, if needed *
+        return;
+    }
+    if (pid == 0) {
+        printf("I am a child: %d PID: %d\n",i, getpid());
+        switch(process)
+        {
+        	case 1:
+        	v = criaVetor(arg);
+			printf("P1: Vetor:          "); imprimeVetor(v, arg);
+			heapsort(v, arg);
+			printf("P1: Vetor ordenado: "); imprimeVetor(v, arg);
+			waitpid(pid, NULL, 0);
+			return;
+
+			case 2:
+			Fibonacci(arg, 0);
+			waitpid(pid, NULL, 0);
+			return;
+
+			case 3:
+			agulhaBuffon(arg);
+			waitpid(pid, NULL, 0);
+			return;
+
+			case 4:
+			integralSecX(sec_arg, arg);
+			waitpid(pid, NULL, 0);
+			return;
+        }
+    }
+
+}*/
 
 
